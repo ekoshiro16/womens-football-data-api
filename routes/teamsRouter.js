5c954027993c8d9ee8ba4bccf8b53f1507278fb2
ekoshiro16 <ekoshiro16@gmail.com>
2024-01-19 15:49:25 -0800

    Added team statistics route

--- routes/teamsRouter.js
+++ routes/teamsRouter.js
@@ -1,6 +1,12 @@
 require("dotenv").config(); 
 const express = require("express");
+const { teams } = require("../data_files/leagueData");
 const teamsRouter = express.Router(); 
+
+const headers = {
+    'X-RapidAPI-Key': process.env.API_KEY,
+    'X-RapidAPI-Host': process.env.API_HOST
+}
 
 teamsRouter.get("/general", async (req, res) => {
     try {
@@ -9,10 +15,7 @@ teamsRouter.get("/general", async (req, res) => {
         const url = `https://api-football-v1.p.rapidapi.com/v3/teams?id=${teamId}`;
 
         const response = await fetch(url, {
-            headers: {
-                'X-RapidAPI-Key': process.env.API_KEY,
-                'X-RapidAPI-Host': process.env.API_HOST
-            }
+            headers
         })
 
         const data = await response.json(); 
@@ -22,6 +25,26 @@ teamsRouter.get("/general", async (req, res) => {
         };
     } catch (e) {
         console.error(e)
+    }
+});
+
+teamsRouter.get("/statistics", async (req, res) => {
+    try {
+        const { leagueId, season, teamId } = req.query; 
+
+        const url = `https://api-football-v1.p.rapidapi.com/v3/teams/statistics?league=${parseInt(leagueId)}&season=${parseInt(season)}&team=${parseInt(teamId)}`;
+
+        const response = await fetch(url, {
+            headers
+        });
+
+        const data = await response.json(); 
+
+        if (data && data?.response) {
+            res.send(data.response); 
+        };
+    } catch (e) {
+        console.error(e); 
     }
 })
 
