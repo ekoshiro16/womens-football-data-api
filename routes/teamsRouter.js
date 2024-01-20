fc908d2a105035c46ff95a501f834a3cc7811266
ekoshiro16 <ekoshiro16@gmail.com>
2024-01-19 16:30:00 -0800

    Added teams squad route

--- routes/teamsRouter.js
+++ routes/teamsRouter.js
@@ -3,9 +3,11 @@ const express = require("express");
 const { teams } = require("../data_files/leagueData");
 const teamsRouter = express.Router(); 
 
-const headers = {
-    'X-RapidAPI-Key': process.env.API_KEY,
-    'X-RapidAPI-Host': process.env.API_HOST
+const metadata = {
+    headers: {
+        'X-RapidAPI-Key': process.env.API_KEY,
+        'X-RapidAPI-Host': process.env.API_HOST
+    }
 }
 
 teamsRouter.get("/general", async (req, res) => {
@@ -14,9 +16,7 @@ teamsRouter.get("/general", async (req, res) => {
 
         const url = `https://api-football-v1.p.rapidapi.com/v3/teams?id=${teamId}`;
 
-        const response = await fetch(url, {
-            headers
-        })
+        const response = await fetch(url, metadata)
 
         const data = await response.json(); 
 
@@ -32,11 +32,9 @@ teamsRouter.get("/statistics", async (req, res) => {
     try {
         const { leagueId, season, teamId } = req.query; 
 
-        const url = `https://api-football-v1.p.rapidapi.com/v3/teams/statistics?league=${parseInt(leagueId)}&season=${parseInt(season)}&team=${parseInt(teamId)}`;
+        const url = `https://api-football-v1.p.rapidapi.com/v3/teams/statistics?league=${leagueId}&season=${season}&team=${teamId}`;
 
-        const response = await fetch(url, {
-            headers
-        });
+        const response = await fetch(url, metadata);
 
         const data = await response.json(); 
 
@@ -46,6 +44,24 @@ teamsRouter.get("/statistics", async (req, res) => {
     } catch (e) {
         console.error(e); 
     }
-})
+});
+
+teamsRouter.get("/squad", async (req, res) => {
+    try {
+        const { teamId } = req.query; 
+
+        const url = `https://api-football-v1.p.rapidapi.com/v3/players/squads?team=${teamId}`;
+
+        const response = await fetch(url, metadata);
+
+        const data = await response.json(); 
+
+        if (data && data?.response) {
+            res.send(data.response); 
+        };
+    } catch (e) {
+        console.error(e); 
+    }
+});
 
 module.exports = teamsRouter; 
